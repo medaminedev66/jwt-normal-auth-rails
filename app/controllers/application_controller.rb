777d27cbe456db82f3cf00ff580e714d4f82adb6
Amine Smahi <md.amine.smahi@gmail.com>
2024-03-09 21:53:42 +0100

    Create Session with login and auto_login actions

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,29 @@ class ApplicationController < ActionController::API
   def encode_token(payload)
     JWT.encode(payload, 'my_secret')
   end
+
+  def session_user
+    decoded_hash = decoded_token
+    if !decode_hash.empty
+      user_id = decoded_hash[0]['user_id']
+      @user = User.find_by(id: user_id)
+    else
+      nil
+    end
+  end
+
+  def auth_header
+    request.header['Authorization']
+  end
+
+  def decoded_token
+    if auth_header
+      token = auth_header.split(' ')[1]
+      begin
+        JWT.decode(token, 'my-secret', true, algorithm: "HS256")
+      rescue JWT::DecodeError
+        []
+      end
+    end
+  end
 end
--- app/controllers/auth_controller.rb
+++ app/controllers/auth_controller.rb
@@ -1,4 +1,6 @@
 class AuthController < ApplicationController
+  before_action :require_login
+
   def login
     user = User.find_by(username: params[:username])
     if user && user.authenticate(params[:password])
@@ -11,8 +13,22 @@ class AuthController < ApplicationController
   end
 
   def auto_login
+    if session_user
+      render json: session_user
+    else
+      render json: {errors: "no user logged in"}
+    end
   end
 
   def user_is_authed
+    
+  end
+
+  def loogged_in?
+    !!session_user
+  end
+  
+  def require_login
+    render json: { message: 'Please login' }, status: unautherized unless logged_in?
   end
 end
